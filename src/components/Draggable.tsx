c06cac9efb911672948d6e1d2175ebb67003ae7d
hon9g <mimmh93@gmail.com>
2022-06-28 16:53:03 +0900

    [dev] Draggable 에서 사용되는 useRef 제네릭 타입 정의
    https://github.com/hon9g/Draggable/issues/2#issuecomment-1168287382

--- src/components/Draggable.tsx
+++ src/components/Draggable.tsx
@@ -8,7 +8,7 @@ interface Props {
 }
 
 const Draggable = ({ children }: Props) => {
-  const draggableElement = useRef();
+  const draggableElement = useRef<HTMLDivElement>(null);
 
   const [state, setState] = useState({
     translate: {x: 0, y: 0},
@@ -18,58 +18,66 @@ const Draggable = ({ children }: Props) => {
   
   const handleDesktopDragStart = useCallback(({clientX, clientY}) => {
     const el = draggableElement.current
-    setState(state => ({
-      ...state,
-      pad: {
-        x: el.clientWidth + state.translate.x - clientX,
-        y: el.clientHeight + state.translate.y - clientY
-      },
-      isDragging: true
-    }))
+    if (el) {
+      setState(state => ({
+        ...state,
+        pad: {
+          x: el.clientWidth + state.translate.x - clientX,
+          y: el.clientHeight + state.translate.y - clientY
+        },
+        isDragging: true
+      }))
+    }
   }, [])
 
   const handleMobileDragStart = useCallback(({touches}) => {
     const el = draggableElement.current
-    setState(state => ({
-      ...state,
-      pad: {
-        x: el.clientWidth + state.translate.x - touches[0].clientX,
-        y: el.clientHeight + state.translate.y - touches[0].clientY
-      },
-      isDragging: true
-    }))
+    if (el) {
+      setState(state => ({
+        ...state,
+        pad: {
+          x: el.clientWidth + state.translate.x - touches[0].clientX,
+          y: el.clientHeight + state.translate.y - touches[0].clientY
+        },
+        isDragging: true
+      }))
+    }
   }, [])
   
   const handleDesktopDragging = useCallback(({clientX, clientY}) => {
     const el = draggableElement.current
-    const translateX = Math.min(
-      window.innerWidth + state.pad.x - el.clientWidth,
-      clientX + state.pad.x - el.clientWidth
-    )
-    const translateY = Math.min(
-      window.innerHeight + state.pad.y- el.clientHeight,
-      clientY + state.pad.y - el.clientHeight
-    )
-    setState(state => ({
-      ...state,
-      translate: {x: translateX, y: translateY}
-    }))
+    if (el) {
+      const translateX = Math.min(
+        window.innerWidth + state.pad.x - el.clientWidth,
+        clientX + state.pad.x - el.clientWidth
+      )
+      const translateY = Math.min(
+        window.innerHeight + state.pad.y- el.clientHeight,
+        clientY + state.pad.y - el.clientHeight
+      )
+      setState(state => ({
+        ...state,
+        translate: {x: translateX, y: translateY}
+      }))
+    }
   }, [state.pad])
 
   const handleMobileDragging = useCallback(({touches}) => {
     const el = draggableElement.current
-    const translateX = Math.min(
-      window.innerWidth  + state.pad.x - el.clientWidth,
-      touches[0].clientX  + state.pad.x - el.clientWidth
-    )
-    const translateY = Math.min(
-      window.innerHeight + state.pad.y - el.clientHeight,
-      touches[0].clientY + state.pad.y - el.clientHeight
-    )
-    setState(state => ({
-      ...state,
-      translate: {x: translateX, y: translateY}
-    }))
+    if (el) {
+      const translateX = Math.min(
+        window.innerWidth  + state.pad.x - el.clientWidth,
+        touches[0].clientX  + state.pad.x - el.clientWidth
+      )
+      const translateY = Math.min(
+        window.innerHeight + state.pad.y - el.clientHeight,
+        touches[0].clientY + state.pad.y - el.clientHeight
+      )
+      setState(state => ({
+        ...state,
+        translate: {x: translateX, y: translateY}
+      }))
+    }
   }, [state.pad])
 
   const handleDragEnd = useCallback(() => {
